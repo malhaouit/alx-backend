be364170ff2e872a16acac2d6089caf833a2be42
malhaouit <abdelmounaim.abdel@gmail.com>
2024-07-30 23:38:39 +0000

    Update to fix a requirement check

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Flask app"""
+"""Flask app with Babel configuration and locale selection."""
 
 from flask import Flask, render_template, request
 from flask_babel import Babel, gettext as _
