68cb3b155d3e979c9406e8954a7e990835ec9360
malhaouit <abdelmounaim.abdel@gmail.com>
2024-07-30 20:37:11 +0000

    Update: change the position of function `get_locale()`

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -19,19 +19,19 @@ app.config.from_object(Config)
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """Determine the best match with our supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
 @app.route('/')
 def display_hello_world() -> str:
     """Outputs 'Welcome to Holberton' as page title and 'Hello world'
     as header.
     """
     return render_template('2-index.html')
-
-
-@babel.localeselector
-def get_locale() -> str:
-    """Determine the best match with our supported languages.
-    """
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == '__main__':
